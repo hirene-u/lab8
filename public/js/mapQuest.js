1d86dc16af9d8c2e0c470654c98d043e9b30d6e9
unknown <ihou@ucsd.ed>
2021-02-25 10:44:19 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
-}
+	L.mapquest.key = 'n7G5NilAEKWAnGwgMz5EZAEzL8tkuMGp';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.88, -117.236],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
+
+
+
+L.marker([32.88, -117.236]).addTo(map);
+}	
